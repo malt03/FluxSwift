e1de8bd8fc87014a2294cb13ea05f2c5f996e6d7
Koji Murata <koji.murata@dena.com>
2020-01-24 13:45:54 +0900

    access control

--- Sources/FluxSwift/Observable+dispatchAction.swift
+++ Sources/FluxSwift/Observable+dispatchAction.swift
@@ -8,15 +8,15 @@
 import RxSwift
 
 extension ObservableType {
-    func dispatchAction<ActionType: Action>(action: @escaping (Element) -> ActionType) -> Disposable where ActionType.StoreType: Store {
+    public func dispatchAction<ActionType: Action>(action: @escaping (Element) -> ActionType) -> Disposable where ActionType.StoreType: Store {
         subscribe(onNext: { action($0).dispatch() })
     }
     
-    func dispatchAction<ActionType: Action>(action: @escaping (Element) -> ActionType) -> Disposable where ActionType.StoreType: IdentifiableStore {
+    public func dispatchAction<ActionType: Action>(action: @escaping (Element) -> ActionType) -> Disposable where ActionType.StoreType: IdentifiableStore {
         subscribe(onNext: { action($0).dispatch() })
     }
     
-    func dispatchAction<ActionType: Action>(id: @escaping (Element) -> ActionType.StoreType.ID, action: @escaping (Element) -> ActionType) -> Disposable where ActionType.StoreType: IdentifiableStore {
+    public func dispatchAction<ActionType: Action>(id: @escaping (Element) -> ActionType.StoreType.ID, action: @escaping (Element) -> ActionType) -> Disposable where ActionType.StoreType: IdentifiableStore {
         subscribe(onNext: { action($0).dispatch(to: id($0)) })
     }
 }
