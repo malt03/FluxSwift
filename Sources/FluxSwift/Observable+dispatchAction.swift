4fb840d23534b4af620820da2f92a36d51a5a5e2
Koji Murata <koji.murata@dena.com>
2020-01-24 14:02:56 +0900

    id

--- Sources/FluxSwift/Observable+dispatchAction.swift
+++ Sources/FluxSwift/Observable+dispatchAction.swift
@@ -19,4 +19,8 @@ extension ObservableType {
     public func dispatchAction<ActionType: Action>(id: @escaping (Element) -> ActionType.StoreType.ID, action: @escaping (Element) -> ActionType) -> Disposable where ActionType.StoreType: IdentifiableStore {
         subscribe(onNext: { action($0).dispatch(to: id($0)) })
     }
+    
+    public func dispatchAction<ActionType: Action>(id: ActionType.StoreType.ID, action: @escaping (Element) -> ActionType) -> Disposable where ActionType.StoreType: IdentifiableStore {
+        subscribe(onNext: { action($0).dispatch(to: id) })
+    }
 }
--- Tests/FluxSwiftTests/ObservableDispatchActionTests.swift
+++ Tests/FluxSwiftTests/ObservableDispatchActionTests.swift
@@ -30,23 +30,30 @@ class ObservableDispatchActionTests: XCTestCase {
         
         let relay = PublishRelay<Int>()
         let disposable = relay.dispatchAction(action: { IdentifiableStore.Set(x: $0) })
-        let identifyRelay = PublishRelay<Int>()
-        let identifyDisposable = identifyRelay.dispatchAction(id: { _ in "a" }, action: { IdentifiableStore.Set(x: $0) })
-        
+        let aRelay = PublishRelay<Int>()
+        let aDisposable = aRelay.dispatchAction(id: "a", action: { IdentifiableStore.Set(x: $0) })
+        let bRelay = PublishRelay<Int>()
+        let bDisposable = bRelay.dispatchAction(id: { _ in "b" }, action: { IdentifiableStore.Set(x: $0) })
+
         XCTAssertEqual(0, a.entity.x)
         XCTAssertEqual(0, b.entity.x)
         relay.accept(1)
         XCTAssertEqual(1, a.entity.x)
         XCTAssertEqual(1, b.entity.x)
-        identifyRelay.accept(2)
+        aRelay.accept(2)
         XCTAssertEqual(2, a.entity.x)
         XCTAssertEqual(1, b.entity.x)
+        bRelay.accept(3)
+        XCTAssertEqual(2, a.entity.x)
+        XCTAssertEqual(3, b.entity.x)
         disposable.dispose()
-        identifyDisposable.dispose()
-        relay.accept(3)
-        identifyRelay.accept(3)
+        aDisposable.dispose()
+        bDisposable.dispose()
+        relay.accept(4)
+        aRelay.accept(4)
+        bRelay.accept(4)
         XCTAssertEqual(2, a.entity.x)
-        XCTAssertEqual(1, b.entity.x)
+        XCTAssertEqual(3, b.entity.x)
     }
     
     struct Store: FluxSwift.Store {
